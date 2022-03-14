cf52484370645ffb06b15ee2ce1771cbb1ba8617
GlinPel <gleenpeltroche2@gmail.com>
2022-03-14 17:42:12 -0500

    primer react

---
+++ src/ToDo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ToDoList from './ToDoList'
+
+const ToDo = () => {
+  return (
+    <div>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ToDoList />
+    </div>
+  )
+}
+
+export default ToDo
---
+++ src/ToDoList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ToDoList = () => {
+  return (
+    <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a Marathon</li>
+        <li>Feed the dogs</li>
+    </ul>
+  )
+}
+
+export default ToDoList
